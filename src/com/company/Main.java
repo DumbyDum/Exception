77636d805c7791eb80421974c93705bdbd63c458
Ad <gadzhiev.adil@mail.ru>
2021-05-28 23:22:54 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws ChessException {
+        throw new ChessException("NO TIME FOR CODING! YOU MUST PLAY CHESS NOW!");
+	// write your code here
+    }
+}
